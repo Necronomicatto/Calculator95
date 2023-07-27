be5006ac1f8d882dcba3b00d5ce9ef2e90bc99bf
Manu Ferreira <Artiotheblack@gmail.com>
2023-07-27 14:49:30 -0300

    (OwO)
    front-end almost finished n' back-end prototype

--- src/main/java/Calculator/Frm.java
+++ src/main/java/Calculator/Frm.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Frm extends JFrame {
+public class Frm extends JFrame implements ActionListener {
     private JTextField txtFld;
     private JButton[] bttns;
     private String[] rdBttnsLbls = {
@@ -19,6 +19,13 @@ public class Frm extends JFrame {
             "4", "5", "6", "*", "%",
             "1", "2", "3", "-", "1/x",
             "0", "+/-", ".", "+", "="
+    };
+    private String[] drkBttnsLbls = {
+        "Back", "CE", "C"
+    };
+
+    private String[] options = {
+        "Edit", "View", "Help"
     };
 
 
@@ -41,13 +48,20 @@ public class Frm extends JFrame {
         pnlI.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
         pnlI.setLayout(null);
 
-        JPanel pnlII = new JPanel();
+        JPanel pnlII = new JPanel();  //number buttons grid
         pnlII.setBounds(108, 150, 274, 170);
         pnlII.setLayout(new GridLayout(4, 5, 6, 6));
 
-        JPanel pnlIII = new JPanel();
+        JPanel pnlIII = new JPanel(); //red buttons grid
         pnlIII.setBounds(18, 103, 55, 218);
         pnlIII.setLayout(new GridLayout(5, 1, 0,6));
+
+        JPanel pnlIV = new JPanel();
+        pnlIV.setBounds(183, 105, 200, 40);
+        pnlIV.setLayout(new GridLayout(1, 3, 6, 0));
+
+        //JPanel pnlV = new JPanel();
+        //pnlV.setBounds(0,0,)
 
         JButton[] rdBttns = new JButton[5];
         rdBttns[0] = new JButton(" ");
@@ -59,7 +73,7 @@ public class Frm extends JFrame {
             rdBttns[i].setBackground(dfltClr);
             rdBttns[i].setForeground(Color.RED);
             rdBttns[i].setBorder(new BevelBorder(BevelBorder.RAISED));
-            //bttns[i].setFont(fnt);
+            rdBttns[i].setFont(fnt);
             //bttns[i].addActionListener(this);
             pnlIII.add(rdBttns[i]);
         }
@@ -70,21 +84,56 @@ public class Frm extends JFrame {
             bttns[i].setBackground(dfltClr);
             bttns[i].setForeground(Color.BLUE);
             bttns[i].setBorder(new BevelBorder(BevelBorder.RAISED));
-            //bttns[i].setFont(fnt);
+            bttns[i].setFont(fnt);
             //bttns[i].addActionListener(this);
             pnlII.add(bttns[i]);
         }
 
+        JButton[] drkBttns = new JButton[drkBttnsLbls.length];
+        for (int i = 0; i < drkBttnsLbls.length; i++){
+            drkBttns[i] = new JButton(drkBttnsLbls[i]);
+            drkBttns[i].setBackground(dfltClr);
+            drkBttns[i].setForeground(Color.RED);
+            drkBttns[i].setBorder(new BevelBorder(BevelBorder.RAISED));
+            drkBttns[i].setFont(fnt);
+            //bttns[i].addActionListener(this);
+            pnlIV.add(drkBttns[i]);
+        }
+
+
         txtFld = new JTextField();
         txtFld.setBounds(18, 18, 364, 36);
         txtFld.setFont(fnt);
+        txtFld.setHorizontalAlignment(JTextField.RIGHT);
         txtFld.setBorder(new BevelBorder(BevelBorder.LOWERED));
 
         pnlI.add(txtFld);
         add(pnlI);
         add(pnlII);
         add(pnlIII);
+        add(pnlIV);
 
         setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String command = e.getActionCommand();
+        if (command.equals("=")) {
+            String expression = txtFld.getText();
+            try {
+                double result = evaluateExpression(expression);
+                txtFld.setText(Double.toString(result));
+            } catch (Exception ex) {
+                txtFld.setText("Error");
+            }
+        } else {
+            txtFld.setText(txtFld.getText() + command);
+        }
+    }
+
+    private double evaluateExpression(String expression) {
+        return Double.parseDouble(expression);
+    }
 }
+
